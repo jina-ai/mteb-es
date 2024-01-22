ff36a93ce05ad32f94fbc28780bebea4357b599f
jupyterjazz <saba.sturua@jina.ai>
2024-01-22 10:38:40 +0100

    feat: import classes
    Signed-off-by: jupyterjazz <saba.sturua@jina.ai>

--- mteb/tasks/Retrieval/__init__.py
+++ mteb/tasks/Retrieval/__init__.py
@@ -41,3 +41,5 @@ from .NarrativeQARetrieval import *
 from .SpanishPassageRetrievalS2S import *
 from .SpanishPassageRetrievalS2P import *
 from .XMarketESRetrieval import *
+from .XPQAESRetrieval import *
+from .MintakaESRetrieval import *
